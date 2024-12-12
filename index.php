f88dec3af0ac1998f6083cfcc85448f1dc6e921e
Ilnur Sirazhev <code_gex@mail.ru>
2024-12-12 16:19:28 +0500

    DKWebsite/classes/22/Отправка данных с веб-сайта в БД

--- index.php
+++ index.php
@@ -15,15 +15,24 @@ const HTML_EOL = "<br>\n";
 </head>
 <body>
 <div class="wrapper-main">
+<!--Создаем форму регистрации с базовыми полями-->
+    <h3>Signup</h3>
+
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username" id="username">
+        <label for="username"></label>
+        <input type="text" name="pwd" placeholder="Password" id="pwd">
+        <label for="pwd"></label>
+        <input type="text" name="email" placeholder="E-Mail" id="email">
+        <label for="email"></label>
+        <button type="submit">Signup</button>
+    </form>
+
 
     <?php
-    // Подключение к БД используя PDO и настройка обработчика ошибок
+    // Отправка данных с веб-сайта через метод POST в БД
 
-    // Создаем файл includes/dbh.inc.php и п создаем в нем PDO подключение
-
-
-
-
+    // Создаем обработчик формы -> создаем файл includes/formhandler.inc.php
 
 
     ?>
