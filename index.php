ebe4f03713724a0db42cb4d1e3a204630c627b55
Ilnur Sirazhev <code_gex@mail.ru>
2024-12-12 14:57:00 +0500

    DKWebsite/classes/20/Подключение к БД используя PDO и настройка обработчика ошибок

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
+const HTML_EOL = "<br>\n";
 ?>
 <!doctype html>
 <html lang="en">
@@ -16,21 +17,10 @@ declare(strict_types=1);
 <div class="wrapper-main">
 
     <?php
-    const HTML_EOL = "<br>\n";
+    // Подключение к БД используя PDO и настройка обработчика ошибок
 
-    // SELECT & JOIN Database Table Data in MySQL
 
-    /* Получение данных из таблицы через SELECT
-    SELECT username, email FROM users WHERE id = 2;
-    SELECT username, comment_text FROM comments WHERE users_id = 1;
-    SELECT * FROM comments WHERE users_id = 1;
 
-    Получение данных через объединение двух таблиц JOIN
-    SELECT * FROM users INNER JOIN comments ON users.id = comments.users_id;
-    SELECT users.username, comments.comment_text, comments.created_at FROM users INNER JOIN comments ON users.id = comments.users_id;
-    SELECT * FROM users LEFT JOIN comments ON users.id = comments.users_id;
-    SELECT * FROM users RIGHT JOIN comments ON users.id = comments.users_id;
-     */
 
 
 
